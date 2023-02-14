9d5ffe6eb98685db0fe99c3686035608e974422b
Eduard Gomá <edugb1408@gmail.com>
2023-02-14 16:19:21 +0100

    Primeras pruebas con terraform

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 terraform {
-	required_providers {
-		aws = {
-		source = "hashicorp/aws"
-		version = "~> 4.16"
-		}	
-	}
-	required_version = ">=1.2.0"
-}	
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">=1.2.0"
+}
 provider "aws" {
-	region = "eu-west-1"
+  region = "eu-west-1"
 }
 resource "aws_instance" "app_server" {
-	ami = "ami-0b752bf1df193a6c4"
-	instance_type = "t2.micro"
-	tags = {
-	  Name = "Example"
-	}
+  ami           = "ami-0b752bf1df193a6c4"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Example"
+  }
 }
