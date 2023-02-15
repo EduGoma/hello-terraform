b78da963352af357962c23cd7f6ec3518e633eec
Eduard Gomá <edugb1408@gmail.com>
2023-02-15 10:18:08 +0100

    Pruebas con user_data para iniciar las maquinas con unas intrucciones ya predefinidas

--- main.tf
+++ main.tf
@@ -17,6 +17,24 @@ resource "aws_instance" "app_server" {
   subnet_id              = "subnet-00ce9b5739c0bcabe"
   vpc_security_group_ids = ["sg-068c536b23394be69"]
   tags = {
-    Name = "Ejemplo"
+    Name = var.instance_name
+    APP  = "vue2048"
   }
+  user_data = <<EOH
+  #!/bin/sh
+  yum update -y
+  amazon-linux-extras install -y docker
+  service docker start
+  systemctl enable docker
+  usermod -a -G docker ec2-user
+  pip3 install docker-compose
+  wget  https://raw.githubusercontent.com/EduGoma/hello-2048/main/docker-compose.yaml
+  mkdir /home/ec2-user/hello-2048
+  chown ec2-user /home/ec2-user/hello-2048
+  sudo chgrp ec2-user /home/ec2-user/hello-2048
+  mv docker-compose.yaml /home/ec2-user/hello-2048/
+  cd /home/ec2-user/hello-2048
+  docker-compose pull
+  docker-compose up -d
+  EOH
 }
