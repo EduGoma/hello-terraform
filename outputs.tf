c691edece3bf47cb25b920398e8f5e40b7fd8bf1
Eduard Gomá <edugb1408@gmail.com>
2023-02-20 09:54:58 +0100

    Añadiendo fichero outputs para salida de datos, y añadiendo un count para que genere 2 instancias por defecto

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "instance_id" {
   description = "ID instance"
-  value       = aws_instance.app_server.id
+  value       = ["${aws_instance.app_server.*.id}"]
 }
 output "instance_ip" {
   description = "Public IP"
-  value       = aws_instance.app_server.public_ip
+  value       = ["${aws_instance.app_server.*.public_ip}"]
 }
